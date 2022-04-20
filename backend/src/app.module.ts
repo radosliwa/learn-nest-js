368ef7e9372b6320bc33bbd7853c2c9447b19041
Radosław Śliwa <sliwa.radoslaw2@gmail.com>
2022-04-20 21:02:17 +0200

    nestjs dockerfile working for dev

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), BlogModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
