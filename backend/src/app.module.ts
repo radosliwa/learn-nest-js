6b2c2c79a3299db68960d5902a4a096ff4da854a
Radosław Śliwa <sliwa.radoslaw2@gmail.com>
2022-04-21 21:21:04 +0200

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), BlogModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_LOCAL), BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
